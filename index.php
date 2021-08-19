cbb543886ec3fcc9411004a7b433817892a271d7
sigmarius <aamokeeva@yandex.ru>
2021-08-19 08:24:41 +0300

    Рефакторит foreach для двумерного массива Products

--- index.php
+++ index.php
@@ -107,18 +107,18 @@ $products = [
         </div>
         <ul class="lots__list">
             <!--список из массива с товарами-->
-            <?php foreach ($products as $product => $value): ?>
+            <?php foreach ($products as $product): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $value['url'] ?>" width="350" height="260" alt="<?= $value['title'] ?>">
+                    <img src="<?= $product['url'] ?>" width="350" height="260" alt="<?= $product['title'] ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $value['category'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $value['title'] ?></a></h3>
+                    <span class="lot__category"><?= $product['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $product['title'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $value['price'] ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $product['price'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
